91a396c0fb39772a2361ea3f5ada541334c2a264
samip779 <samipluitel28@gmail.com>
2023-12-02 19:14:38 +0545

    add swagger docs and set global api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './environments';
+import { MODE, PORT } from './environments';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { DataSource } from 'typeorm';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/v1/api');
+
+  if (MODE === 'dev') {
+    // OpenAPI integration
+    const config = new DocumentBuilder()
+      .setTitle('IDonar')
+      .setDescription('IDonar API Specification')
+      .setVersion('1.0.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
 
   // global validation pipe
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
