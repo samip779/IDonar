206a5a2f6b4f353ac2c1507fd85949291a543546
samip779 <samipluitel28@gmail.com>
2023-12-02 17:03:13 +0545

    remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DATABASEENV } from './environments';
 
@@ -18,7 +16,7 @@ import { DATABASEENV } from './environments';
       synchronize: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
