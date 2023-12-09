f11be63d4fd3b38e46f2e02a900adf1ef4823937
samip779 <samipluitel28@gmail.com>
2023-12-09 19:26:22 +0545

    add bearer auth in swagger

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
       .setTitle('IDonar')
       .setDescription('IDonar API Specification')
       .setVersion('1.0.0')
+      .addBearerAuth()
       .build();
 
     const document = SwaggerModule.createDocument(app, config);
