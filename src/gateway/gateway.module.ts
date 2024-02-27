755dac20482ec13c304a5a787cb18aa68fe2e4da
samip779 <samipluitel28@gmail.com>
2024-02-27 17:17:35 +0545

    add socket configuration

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Gateway } from './gateway';
 import { SubscribeMessage } from '@nestjs/websockets';
+import { GatewayService } from './gateway.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [Gateway],
+  imports: [JwtModule.register({}), UsersModule],
+  providers: [Gateway, GatewayService],
 })
 export class GatewayModule {
   @SubscribeMessage('message')
--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -6,19 +6,55 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { GatewayService } from './gateway.service';
+import { JwtService } from '@nestjs/jwt';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+import { JWTSECRET } from '../environments';
+import { UsersService } from '../users/users.service';
 
 @WebSocketGateway()
 export class Gateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor() {}
+  constructor(
+    private gatewayService: GatewayService,
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @WebSocketServer()
   private server: Server;
 
-  afterInit(server: any) {}
+  afterInit(server: any) {
+    this.gatewayService.server = server;
+  }
 
-  handleConnection(client: Socket) {}
+  async handleConnection(client: Socket) {
+    try {
+      const token =
+        client.handshake.auth.token || client.handshake.headers.authorization;
+
+      if (!token) throw new UnauthorizedException();
+
+      const decoded = await this.jwtService.verifyAsync(token, {
+        secret: JWTSECRET,
+      });
+
+      const user = await this.usersService.findOneBy(
+        { id: decoded.sub },
+        { id: true },
+      );
+
+      if (!user) throw new UnauthorizedException();
+
+      client.data = { userId: user.id };
+      client.join(user.id);
+      client.emit('socket-status', { connected: true, status: 200 });
+    } catch (error) {
+      client.emit('socket-status', { connected: false, status: 404 });
+      return client.disconnect();
+    }
+  }
 
   handleDisconnect(client: Socket) {}
 }
